4963e8cafbea17e745b1aa431aeecf6563641b34
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-22 11:46:37 +1200

    Hm

--- ci-build.sh
+++ ci-build.sh
@@ -26,9 +26,9 @@ for i in "${modulesToBuild[@]}"; do
         file=${filename##*/}
         name=${file%%.*}
         echo "Building $name.html"
-        jagregory/pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
+        pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
         echo "Building $name.pdf"
-        jagregory/pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+        pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
     done
 
     # Put the HTML slides and
