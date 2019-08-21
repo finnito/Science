996d8b26d13630c4035a3a5c06d8456ca5237e6e
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-21 20:54:39 +1200

    Bring in build script from separate repo

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# Check if an argument is given
+# If no argument is give, build all MD files,
+# else build only the slide given.
+if [ $# -eq 0 ]
+    then
+        for filename in markdown/*.md; do
+            file=${filename##*/}
+            name=${file%%.*}
+            pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
+            pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+        done
+else
+    filename=$(find ./markdown -name "$1*")
+    file=${filename##*/}
+    name=${file%%.*}
+    pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html" -V revealjs-url=../reveal.js
+    pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+fi
+
+# Put files in correct places
+mv *.html slides/
+mv *.pdf pdfs/
+cp -r assets/ slides/assets
+
+# Generate new ZIP
+topic=${PWD##*/}
+cd pdfs
+zip "$topic".zip *
+mv *.zip ../
