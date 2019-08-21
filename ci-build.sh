c490220d870b2a19b582d969e8a60a2415d2741b
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-21 23:43:53 +1200

    Create CI build script and add to CI config

---
+++ ci-build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Ensure the script runs from
+# the directory it's located in.
+scriptDir="$(dirname "$0")"
+cd $scriptDir
+
+modulesToBuild=(
+    "content/10scie/5-fire-and-fuels"
+    "content/10scie/6-geology"
+    "content/11sci/4-mechanics"
+    "content/11sci/5-genetics"
+    "content/12phy/2-mechanics"
+    "content/12phy/3-electricity"
+)
+
+for i in "${modulesToBuild[@]}"; do
+
+    # Navigate to the directory.
+    echo "Navigating to $i"
+    cd $i
+
+    # Build the HTML slides and
+    # PDFs for all markdown docs.
+    for filename in markdown/*.md; do
+        file=${filename##*/}
+        name=${file%%.*}
+        echo "Building $name.html"
+        pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
+        echo "Building $name.pdf"
+        pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+    done
+
+    # Put the HTML slides and
+    # PDFs into the right place.
+    echo "Moving slides & PDFs"
+    mv *.html slides/
+    mv *.pdf pdfs/
+
+    # Create a ZIP of the PDFs
+    # available for download.
+    echo "Creating .zip"
+    topic=${PWD##*/}
+    cd pdfs
+    zip "$topic".zip *
+    mv *.zip ../
+    cd ../
+
+    # Head back to the script
+    # directory to do the next
+    # module.
+    echo "Navigating back to top level"
+    echo ""
+    cd ../../../
+done
