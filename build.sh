8ee10407b3efcb85745646f746c64342cd6f681d
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-22 09:12:59 +1200

    Fix command

--- build.sh
+++ build.sh
@@ -13,15 +13,15 @@ if [ $# -eq 0 ]
         for filename in markdown/*.md; do
             file=${filename##*/}
             name=${file%%.*}
-            pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
-            pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+            jpbernius/pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
+            jpbernius/pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
         done
 else
     filename=$(find ./markdown -name "$1*")
     file=${filename##*/}
     name=${file%%.*}
-    pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html" -V revealjs-url=../reveal.js
-    pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+    jpbernius/pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html" -V revealjs-url=../reveal.js
+    jpbernius/pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
 fi
 
 # Put files in correct places
