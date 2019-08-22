64d480cd2b24a7d58f3ebbad8141bcc7df3e81e1
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-23 11:09:03 +1200

    Meep

--- ci-build.sh
+++ ci-build.sh
@@ -21,9 +21,9 @@ for i in "${modulesToBuild[@]}"; do
         file=${filename##*/}
         name=${file%%.*}
         echo "Building $name.html"
-        pandoc/latex pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
+        /usr/local/bin/pandoc -s --mathjax -i -t revealjs "markdown/$name.md" -o "$name.html"
         echo "Building $name.pdf"
-        pandoc/latex pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
+        /usr/local/bin/pandoc "markdown/$name.md" -o "$name.pdf" --pdf-engine=pdflatex
     done
 
     # Put the HTML slides and
