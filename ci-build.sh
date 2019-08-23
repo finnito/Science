b04558a27f0acb1094381128397150e8c3ca8f9a
Finn LeSueur <finn.lesueur@gmail.com>
2019-08-23 15:44:14 +1200

    Hm

--- ci-build.sh
+++ ci-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 modulesToBuild=(
-    "/builds/Finnito/science/content/10sci/5-fire-and-fuels"
-    "/builds/Finnito/science/content/10scie/6-geology"
-    "/builds/Finnito/science/content/11sci/4-mechanics"
-    "/builds/Finnito/science/content/11sci/5-genetics"
-    "/builds/Finnito/science/content/12phy/2-mechanics"
-    "/builds/Finnito/science/content/12phy/3-electricity"
+    "builds/Finnito/science/content/10sci/5-fire-and-fuels"
+    "builds/Finnito/science/content/10scie/6-geology"
+    "builds/Finnito/science/content/11sci/4-mechanics"
+    "builds/Finnito/science/content/11sci/5-genetics"
+    "builds/Finnito/science/content/12phy/2-mechanics"
+    "builds/Finnito/science/content/12phy/3-electricity"
 )
 
 for i in "${modulesToBuild[@]}"; do
@@ -40,4 +40,6 @@ for i in "${modulesToBuild[@]}"; do
     zip "$topic".zip *
     mv *.zip ../
     cd ../
+
+    cd ../../../../../
 done
