0dc598a5fc2bb9945e07748c3981dba60431cc01
Finn LeSueur <finn.lesueur@gmail.com>
2019-09-10 10:31:35 +1200

    Add post-build script to put assets in correct place

---
+++ post-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Navigating to content"
+cd public
+
+modulesToBuild=(
+    "10scie/5-fire-and-fuels"
+    "10scie/6-geology"
+    "11sci/4-mechanics"
+    "11sci/5-genetics"
+    "12phy/2-mechanics"
+    "12phy/3-electricity"
+)
+
+for i in "${modulesToBuild[@]}"; do
+
+    # Navigate to the directory.
+    echo "Navigating to $i"
+    cd $i
+    # ls
+
+    echo "Copying assets into slides"
+    find slides -type d -maxdepth 1 -exec cp -r assets {} \;
+
+    echo "Should be back in content"
+    cd ../../
+    # ls
+done
