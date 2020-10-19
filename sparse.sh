9b7a2b9e1c8dd3b5f972e21585cd96a1cc8290eb
Finn Le Sueur <finn@lesueur.nz>
2020-10-20 01:51:55 +1300

    (Fix): Use long flags and only watch content directory

--- sparse.sh
+++ sparse.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-fswatch -x0re ".*" -i "\\.md$" ./ | xargs -0 -n 1 python3 sparse-build.py
+fswatch --print0 \
+        --event-flags \
+        --recursive \
+        --exclude=".*" \
+        --include="\\.md$" \
+        ./content \
+        | xargs -0 -n 1 bash build.sh --dev
