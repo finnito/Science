2fb265df76e5f43dd61f3195d97e453b4f0db27d
Finn Le Sueur <finn@lesueur.nz>
2020-12-01 10:16:44 +1300

    Starts build script in Python

---
+++ build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" A Python script that controls the
+CI build process of https://putaiao.lesueur.nz
+"""
+
+def main():
+    """ The brains. """
+    print("Hello!")
+
+if __name__ == "__main__":
+    main()
