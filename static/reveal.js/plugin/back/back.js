2aa3f02af82da1bd1a85daca04fac63358155273
Finn Le Sueur <finn@lesueur.nz>
2021-01-02 12:30:25 +1300

    Chore: Change text in slides back button

--- static/reveal.js/plugin/back/back.js
+++ static/reveal.js/plugin/back/back.js
@@ -6,7 +6,7 @@
 var GoBack = window.GoBack || (function(){
 	return {
 		init: function() {
-			var button = "<a class='back-button' title='Back to Unit' href='../'>Back to Unit</a>";
+			var button = "<a class='back-button' title='Exit Slides' href='../'>Exit</a>";
 			var reveal = document.querySelector(".reveal");
 			reveal.innerHTML = button + reveal.innerHTML;
 		}
