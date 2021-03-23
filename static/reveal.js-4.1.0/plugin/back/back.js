f8b82eb9c13fcacc1fab23f9d27bba8a378246d8
Finn Le Sueur <finn@lesueur.nz>
2021-03-24 12:15:27 +1300

    Remove unnecessary console.log

--- static/reveal.js-4.1.0/plugin/back/back.js
+++ static/reveal.js-4.1.0/plugin/back/back.js
@@ -6,7 +6,6 @@
 window.GoBack = window.GoBack || {
 	id: 'GoBack',
 	init: function(deck){
-		console.log("Adding back button");
 		var button = "<a class='back-button' title='Exit Slides' href='../'>Exit</a>";
 		var reveal = document.querySelector(".reveal");
 		reveal.innerHTML = button + reveal.innerHTML;
