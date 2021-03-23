23c8420b8bdafa346e5a8fc2345a9d27eae459cf
Finn Le Sueur <finn@lesueur.nz>
2021-03-23 14:01:14 +1300

    Adds script to generate lesson plan outlines

---
+++ lesson-plan-generator.py
@@ -0,0 +1,16 @@
+lessons = 26
+
+output = "### Ngā Mahere Akoranga: _Lesson Plans_\n\n"
+
+for i in range(0, lessons):
+    output += f"""{i+1}. #### 
+    - __Whakaritenga__
+        - 
+    - __Te Whāinga Ako__
+        1. 
+    - __Notes__
+        - Slides: 
+    - __Task/Ngohe__
+        - \n\n"""
+
+print(output)
