b73fe4ff58ee4e48d19c48df1c70a2ef06788fe8
Finn Le Sueur <finn@lesueur.nz>
2021-05-28 13:48:51 +1200

    Update the lesson plan generator

--- lesson-plan-generator.py
+++ lesson-plan-generator.py
@@ -1,16 +1,16 @@
-lessons = 26
+lessons = int(input("Number of lessons: "))
 
 output = "### Ngā Mahere Akoranga: _Lesson Plans_\n\n"
 
 for i in range(0, lessons):
     output += f"""{i+1}. #### 
     - __Whakaritenga__
-        - 
+        + 
     - __Te Whāinga Ako__
         1. 
     - __Notes__
-        - Slides: 
+        + Slides: 
     - __Task/Ngohe__
-        - \n\n"""
+        + \n\n"""
 
 print(output)
