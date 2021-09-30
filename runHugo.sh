87cd0088f59502262f310dbe2b07011e6df94886
Finn Le Sueur <finn@lesueur.nz>
2021-09-30 15:19:39 +1300

    Chore: Improve the dev scripts

--- runHugo.sh
+++ runHugo.sh
@@ -2,9 +2,13 @@
 
 source ~/.profile
 
+dest=~/Sites/public.putaiao.nz/
+echo "$dest"
+
 hugo serve \
 	--cleanDestinationDir \
 	--disableFastRender \
 	--forceSyncStatic \
 	--gc \
-	--noHTTPCache
+	--noHTTPCache \
+	--destination="$dest"
--- watch.sh
+++ watch.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+# The purpose of this script is to use fswatch
+# to look for changes to files in this project.
+#
+# E.g. if a slideshow is updated/created/deleted
+# then the event is fired off to build.sh where
+# the new RevealJS HTML file can be created
+# using Pandoc.
+
 source ~/.profile
 
 terminal-notifier \
         -group "com.finnlesueur.science" \
         -title "Pūtaiao" \
         -message "fswatch started!" \
-        -appIcon http://putaiao.test/favicon.png
+        -appIcon http://localhost:1313/favicon.png
 
 fswatch --print0 \
         --event-flags \
