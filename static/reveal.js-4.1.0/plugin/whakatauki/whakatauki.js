03c5c7856108df9813245fab4db2e4ef70d62354
Finn Le Sueur <finn@lesueur.nz>
2022-02-02 08:36:44 +1300

    Improve week-whakatauki detection

--- static/reveal.js-4.1.0/plugin/whakatauki/whakatauki.js
+++ static/reveal.js-4.1.0/plugin/whakatauki/whakatauki.js
@@ -100,42 +100,40 @@ window.Whakatauki = window.Whakatauki || {
             case 13: i = 7; break;
             case 14: i = 8; break;
             case 15: i = 9; break;
+            case 16: i = 10; break;
 
             // Term 2
-            case 16: i = 10; break;
-            case 17: i = 11; break;
-            case 18: i = 12; break;
-            case 19: i = 13; break;
-            case 20: i = 14; break;
-            case 21: i = 15; break;
-            case 22: i = 16; break;
-            case 23: i = 17; break;
-            case 24: i = 18; break;
-            case 25: i = 19; break;
+            case 19: i = 11; break;
+            case 20: i = 12; break;
+            case 21: i = 13; break;
+            case 22: i = 14; break;
+            case 23: i = 15; break;
+            case 24: i = 16; break;
+            case 25: i = 17; break;
+            case 26: i = 18; break;
+            case 27: i = 19; break;
+            case 28: i = 20; break;
 
             // Term 3
-            case 26: i = 20; break;
-            case 27: i = 21; break;
-            case 28: i = 22; break;
-            case 29: i = 23; break;
-            case 30: i = 24; break;
-            case 31: i = 25; break;
-            case 32: i = 26; break;
-            case 33: i = 27; break;
-            case 34: i = 28; break;
-            case 35: i = 29; break;
+            case 31: i = 21; break;
+            case 32: i = 22; break;
+            case 33: i = 23; break;
+            case 34: i = 24; break;
+            case 35: i = 25; break;
+            case 36: i = 26; break;
+            case 37: i = 27; break;
+            case 38: i = 28; break;
+            case 39: i = 29; break;
 
             // Term 4
-            case 36: i = 30; break;
-            case 37: i = 31; break;
-            case 38: i = 32; break;
-            case 39: i = 33; break;
-            case 40: i = 34; break;
-            case 41: i = 35; break;
-            case 42: i = 36; break;
-            case 43: i = 37; break;
-            case 44: i = 38; break;
-            case 45: i = 39; break;
+            case 43: i = 30; break;
+            case 44: i = 31; break;
+            case 45: i = 32; break;
+            case 46: i = 33; break;
+            case 47: i = 34; break;
+            case 48: i = 35; break;
+            case 49: i = 36; break;
+            case 50: i = 37; break;
 
             default: i = 0; break;
         }
