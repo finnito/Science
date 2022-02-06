ce2532192b1ddf9244f97f1a0532d6b035ec9a39
Finn Le Sueur <finn@lesueur.nz>
2022-02-07 10:06:06 +1300

    Attempts to add a better week-of-the-year detection function

--- static/reveal.js-4.1.0/plugin/whakatauki/whakatauki.js
+++ static/reveal.js-4.1.0/plugin/whakatauki/whakatauki.js
@@ -82,10 +82,14 @@ window.Whakatauki = window.Whakatauki || {
             ]
         ];
 
-        currentdate = new Date();
-        var oneJan = new Date(currentdate.getFullYear(),0,1);
-        var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-        var weekNumber = Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7);
+        var today = new Date();
+        var weekNumber = today.getWeek();
+        console.log(weekNumber);
+        // currentdate = new Date();
+        // var oneJan = new Date(currentdate.getFullYear(),0,1);
+        // var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+        // var weekNumber = Math.ceil(numberOfDays / 7);
+        // console.log(numberOfDays, weekNumber);
 
         var i;
         switch (weekNumber) {
@@ -144,3 +148,15 @@ window.Whakatauki = window.Whakatauki || {
         reveal.innerHTML = "<p class='whakatauki'>" + whakatauki[i][0] + "<br/><em>" + whakatauki[i][1] + "</em></p>" + reveal.innerHTML;
     }
 };
+
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
